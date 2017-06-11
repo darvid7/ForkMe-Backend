8012e9b426424b996e1287f01433e07e414da374
darvid7 <bvdarvid@gmail.com>
2017-06-11 12:48:17 +1000

    Add Create Table developers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 var geocoder = require('geocoder');
 var pg = require('pg');
 
+// App/Middleware set up.
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -11,7 +12,6 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-
 app.listen(port, () => {
 	console.log("Server running on port " + port);
 });
@@ -19,6 +19,28 @@ app.listen(port, () => {
 // Load data stores.
 var repositories = require('./app/store/repositories');
 var developers = require('./app/store/developers');
+
+// Postgres database set up.
+const dbConnectionUrl = 'postgres://localhost:5432/';
+const client = new pg.Client(dbConnectionUrl);
+client.connect();
+
+// CREATE TABLE IF NOT EXIST
+// Precondition: This happens before any end points triggered by clients.
+// Create table if it doesn't already exist, only happens once.
+client
+	.query(
+    	'CREATE TABLE IF NOT EXISTS developers(' +
+    	'login VARCHAR(500) PRIMARY KEY,' +
+    	'email VARCHAR(500),' +
+    	'city VARCHAR(500) not null,' +
+    	'message VARCHAR(500) not null,' + 
+    	'name VARCHAR(500) not null)'
+    	)
+	.on('end', () => { 
+		console.log('Created table developers');
+	});
+
 
 // Routes.
 app.get('/', (req, res) => {
