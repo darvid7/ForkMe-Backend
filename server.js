6be00e4755245f4873a5695eb060a09a9cb99b27
darvid7 <bvdarvid@gmail.com>
2017-06-11 11:48:17 +1000

    Add server and end points serving static files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8080;
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
+app.listen(port, () => {
+	console.log("Server running on port " + port);
+});
+
+// Load data stores.
+var repositories = require('./app/store/repositories');
+var developers = require('./app/store/developers');
+
+// Routes.
+app.get('/', (req, res) => {
+	res.send("Hi, im running on port: " + port);
+});
+
+app.get('/repositories', (req, res) => {
+	res.send(repositories);
+});
+
+app.get('/developers/:login', (req, res) => {
+	res.send(developers);
+})
+
+/*
+Assume post request body is of form:
+{email: 'validemail@email.com', latitude: -37.123414, login: 'username', longitude: 145.12341, message: 'my message', name: 'my name'}
+Gets formatted city address and inserts into database, if error tries update, if further error it propagates up.
+*/
+app.post('/locations', (req, res) => {
+	console.log("\nLocations post request");
+	console.log(req.body);
+});
+
